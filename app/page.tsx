7389fba39cf373fb61bf5de3371af8880e260763
Josinho Ka <josinhoka@gmail.com>
2026-06-08 10:09:11 +0200

    Create initial portfolio homepage

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+      <main>
+        <h1>José Carlos Lorente</h1>
+        <p>Backend & Software Developer</p>
+      </main>
+    
+  );
+}
